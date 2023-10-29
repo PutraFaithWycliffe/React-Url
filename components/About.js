97eefe25e1ada03545a51419d930655a0c500ffa
Putralombu <putralombu@gmail.com>
2023-10-30 01:18:05 +0800

    exercise1

---
+++ components/About.js
@@ -0,0 +1,10 @@
+//about page
+function About() {
+  return (
+    <div>
+      <h2>Front-end web development is the development of the graphical user interface of a website, through the user of HTML, CSS, and JavaScript, so that users can view and interact with that website.</h2>
+      Reac more the article:
+      <a href="https://frontendmasters.com/guides/front-end-handbook/2019/">https://frontendmasters.com/guides/front-end-handbook/2019/</a>
+    </div>
+  );
+}
